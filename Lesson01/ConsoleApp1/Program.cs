38f0f9f4f841190d0bcce36b549ca98c9264b488
vadim2071 <vadim207138@gmail.com>
2022-02-05 00:02:40 +0300

    End

--- Lesson01/ConsoleApp1/Program.cs
+++ Lesson01/ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -8,193 +10,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /*var result = Task.WhenAll(getPostId4(), getPostId5(), 
-                                      getPostId6(), getPostId7(), 
-                                      getPostId8(), getPostId9(), 
-                                      getPostId10(), getPostId11(), 
-                                      getPostId12(), getPostId13());*/
-            var result = Task.WhenAll(getPostId("4"), getPostId("5"), getPostId("6"));
+            var result = Task.WhenAll(getPostId(4), getPostId(5), 
+                                      getPostId(6), getPostId(7), 
+                                      getPostId(8), getPostId(9), 
+                                      getPostId(10), getPostId(11), 
+                                      getPostId(12), getPostId(13));
 
             var array = result.Result;
-            foreach (var item in array)  Console.WriteLine(item);
+
+            string writePath = @"c:\test\result.txt";
+            if (File.Exists(writePath)) File.Delete(writePath);
+
+            Post post = new Post();
+
+            foreach (var item in array)
+            {
+                post = JsonConvert.DeserializeObject<Post>(item);
+                using (StreamWriter sw = new StreamWriter(writePath, true))
+                {
+                    sw.WriteLine(post.userId);
+                    sw.WriteLine(post.id);
+                    sw.WriteLine(post.title);
+                    sw.WriteLine(post.body);
+                    sw.WriteLine();
+                    sw.Close();
+                }
+
+            }
 
         }
-        HttpClient client = new HttpClient();
-        
 
-        //static readonly 
-        static HttpClient httpClient = new HttpClient();
         static Uri Uri = new Uri("https://jsonplaceholder.typicode.com");
         
-
-        static async Task<string> getPostId(string id)
+        static async Task<string> getPostId(int id)
         {
-            try
-            {
-                //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "/posts/{id}");
-                //HttpCompletionOption option = new HttpCompletionOption();
-                //option = HttpCompletionOption.ResponseHeadersRead;
-                httpClient.BaseAddress = Uri;
-                // HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com");
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"/posts/{id}");
-                //response.EnsureSuccessStatusCode();
-                //string responseBody = await response.Content.ReadAsStringAsync();
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = Uri;
+            HttpResponseMessage response = await httpClient.GetAsync($"/posts/{id}");
+            string responseBody = await response.Content.ReadAsStringAsync();
 
-                //request.EnsureSuccessStatusCode();
-                string responseBody = await request.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
+            return responseBody;
         }
 
-
-
-
-
-
-        /*static async Task<string> getPostId4()
+        class Post
         {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/4");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
+            public string userId { get; set; }
+            public string id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
         }
-        static async Task<string> getPostId5()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/5");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId6()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/6");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId7()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/7");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId8()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/8");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId9()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/9");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId10()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/10");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId11()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/11");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId12()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/12");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        static async Task<string> getPostId13()
-        {
-            try
-            {
-                HttpResponseMessage response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/13");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch
-            {
-                return null;
-            }
-        }*/
     }
 
 }
