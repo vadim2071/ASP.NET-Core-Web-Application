4ae070b3c0490de9ba0c0c3737232ce6ec6bc2cc
vadim2071 <vadim207138@gmail.com>
2022-02-10 23:46:16 +0300

    +

--- WebAPI/TimeSheets.BD/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimeSheets.BD
-{
-    public class Class1
-    {
-    }
-}
---
+++ WebAPI/TimeSheets.BD/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSheets.BD
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public int Age { get; set; }
+    }
+}
